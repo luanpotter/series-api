7ba064afacccd99fde4f3264ee7f300a6b475ddd
Luan Nico <luanpotter27@gmail.com>
2019-03-08 22:13:10 -0300

    proper parsing data from season

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const express = require('express');
 
 const app = express();
 
+const { query } = require('./parser');
+
 app.get('/hello', (req, res) => {
     const data = `Hello, ${req.path}`;
     res.status(200).send(data);
+});
+
+app.get('/query', (req, resp) => {
+    const data = { title: req.query.title, season: req.query.season };
+    query(data).then(result => resp.status(200).send(JSON.stringify(result)));
 });
 
 module.exports.handler = serverless(app);
---
+++ parser.js
@@ -0,0 +1,31 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+const listingUrl = ({ title, season }) => `https://www.imdb.com/title/${title}/episodes?season=${season}`;
+const titlePageUrl = id => `https://www.imdb.com/title/${id}`;
+
+const extractImdbId = str => /^\/title\/([^/]*)\/.*$/g.exec(str)[1];
+
+const query = async info => {
+    const listingHtml = await rp(listingUrl(info));
+    const episodes = Array.from($('#episodes_content div.list.detail.eplist div.list_item', listingHtml));
+    const response = episodes.map(tag => ({
+        episodeNumber: $('.info meta[itemprop=episodeNumber]', tag).attr('content').trim(),
+        imdbId: extractImdbId($('.info > strong a', tag).attr('href').trim()),
+        title: $('.info > strong a', tag).text().trim(),
+        releaseDate: $('.info div.airdate', tag).text().trim(),
+    })).map(async el => {
+        const titlePageHtml = await rp(titlePageUrl(el.imdbId));
+        const duration = $('#title-overview-widget .title_block .title_wrapper .subtext > time', titlePageHtml).text().trim();
+        return {
+            ...el,
+            duration,
+        };
+    });
+    return await Promise.all(response);
+};
+
+module.exports = { query };
