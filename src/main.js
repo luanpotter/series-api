015cbf194031c4a4f808cbd7deb5302c54c45724
Luan Nico <luanpotter27@gmail.com>
2019-05-01 23:41:44 -0300

    organize files

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const { adminRoutes } = require('./admin');
+const { read } = require('./storage');
+
+const { API_URL } = require('./env');
+
+const mainRoutes = app => {
+    app.get('/', (_, res) => {
+        const data = `Hello, and welcome to the Series API! My URL: ${API_URL}`;
+        res.status(200).send(data);
+    });
+
+    app.get(/^\/series(\/.*)?$/, async (req, res) => {
+        try {
+            const url = req.url.replace(/^\//g, '');
+            console.log(`Requesting ${url}`);
+            const data = await read(url);
+            console.log(`Responding ${data}`);
+            res.status(200).type('json').send(data);
+        } catch (ex) {
+            console.error('Could not found file', ex);
+            res.status(404).send('Not found');
+        }
+    });
+};
+
+module.exports = { mainRoutes };
+
