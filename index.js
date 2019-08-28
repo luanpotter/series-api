77ad72ad6b458b33ba2ed9752b4768b440ba795f
Luan Nico <luanpotter27@gmail.com>
2019-08-28 13:14:13 -0300

    enable cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const cors = require('cors');
 
 const { routes } = require('./src/app');
 
 const app = express();
+app.use(cors());
 routes(app);
 
 module.exports.handler = serverless(app);
